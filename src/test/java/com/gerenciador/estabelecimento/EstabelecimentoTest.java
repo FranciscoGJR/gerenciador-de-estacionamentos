f235292dff476715412eced71a6b50dd295f5962
FranciscoGJR <franciscoogjr@gmail.com>
2023-10-12 16:35:59 -0300

    feature: adicionando classes para teste de Estabelecimento e Veiculos

---
+++ src/test/java/com/gerenciador/estabelecimento/EstabelecimentoTest.java
@@ -0,0 +1,14 @@
+package com.gerenciador.estabelecimento;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EstabelecimentoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/gerenciador/veiculo/VeiculoTest.java
@@ -0,0 +1,14 @@
+package com.gerenciador.veiculo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VeiculoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
