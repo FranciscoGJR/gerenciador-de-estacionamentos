5cd422e4332f91a95f25d40204e69cceecb33391
FranciscoGJR <franciscoogjr@gmail.com>
2023-10-12 18:26:50 -0300

    test: implementado EstabelecimentoTest.testGetNome()

--- src/test/java/com/gerenciador/estabelecimento/EstabelecimentoTest.java
+++ src/test/java/com/gerenciador/estabelecimento/EstabelecimentoTest.java
@@ -1,14 +1,29 @@
 package com.gerenciador.estabelecimento;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+import model.Estabelecimento;
+
+@TestInstance(Lifecycle.PER_CLASS)
 class EstabelecimentoTest {
 
+	private Estabelecimento estabelecimentoPadrao;
+	
+	@BeforeAll
+	void definirEstabelecimento() {
+		estabelecimentoPadrao = new Estabelecimento("Loja A", "Av. Brasil", 20, 100);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetNome() {
+		Estabelecimento estabelecimento = new Estabelecimento("Loja A", "Av. Brasil", 20, 100);
+		String nome = estabelecimento.getNome();
+		Assertions.assertEquals("Loja A", nome);
+		
 	}
 
 }
