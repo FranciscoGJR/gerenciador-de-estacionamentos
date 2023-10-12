134bfa0da1084bb7677adaaeb6c5824632bbb8a6
FranciscoGJR <franciscoogjr@gmail.com>
2023-10-12 17:28:32 -0300

    feature: implementado atributos, getters e setters de Estabelecimento

--- src/main/java/model/Estabelecimento.java
+++ src/main/java/model/Estabelecimento.java
@@ -1,5 +1,44 @@
 package model;
 
 public class Estabelecimento {
+	
+	String nome;
+	
+	String endereco;
+	
+	Integer quantidadeVagasMotos;
+	
+	Integer quantidadeVagasCarros;
 
+	public Estabelecimento(String nome, String endereco, Integer quantidadeVagasMotos, Integer quantidadeVagasCarros) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.quantidadeVagasMotos = quantidadeVagasMotos;
+		this.quantidadeVagasCarros = quantidadeVagasCarros;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Integer getQuantidadeVagasCarros() {
+		return quantidadeVagasCarros;
+	}
+
+	public void setQuantidadeVagasCarros(Integer quantidadeVagasCarros) {
+		this.quantidadeVagasCarros = quantidadeVagasCarros;
+	}
+	
 }
