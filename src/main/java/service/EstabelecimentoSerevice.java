55ddcb0fae344cc188c914bcd13f342313034605
FranciscoGJR <franciscoogjr@gmail.com>
2023-10-13 18:02:17 -0300

    feature: definindo metodos de VeiculoService

--- src/main/java/service/EstabelecimentoSerevice.java
+++ src/main/java/service/EstabelecimentoSerevice.java
@@ -1,5 +1,39 @@
 package service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import model.Estabelecimento;
+import repository.EstabelecimentoRepository;
+
 public class EstabelecimentoSerevice {
+	
+	@Autowired
+	EstabelecimentoRepository estabelecimentoRepository;
+	
+	public List<Estabelecimento> getEstabelecimento() {
+		return null;
+	}
+
+	public Estabelecimento save() {
+		return null;
+	}
+
+	public Estabelecimento findById() {
+		return null;
+	}
+
+	public List<Estabelecimento> findByNome() {
+		return null;
+	}
+
+	public void update() {
+		return;
+	}
+
+	public void delete() {
+		return;
+	}
 
 }
--- src/main/java/service/VeiculoService.java
+++ src/main/java/service/VeiculoService.java
@@ -1,5 +1,41 @@
 package service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Veiculo;
+import repository.VeiculoRepository;
+
+@Service
 public class VeiculoService {
+	
+	@Autowired
+	private VeiculoRepository veiculoRepository;
+	
+	public List<Veiculo> getVeiculo() {
+		return null;
+	}
+
+	public Veiculo save() {
+		return null;
+	}
+
+	public Veiculo findById() {
+		return null;
+	}
+
+	public List<Veiculo> findByNome() {
+		return null;
+	}
+
+	public void update() {
+		return;
+	}
+
+	public void delete() {
+		return;
+	}
 
 }
