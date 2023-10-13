217d9582f831f34f60f775e7512d80f45231a2ac
FranciscoGJR <franciscoogjr@gmail.com>
2023-10-13 17:53:06 -0300

    feature: implementando anotacoes nos compos Estabelecimento

--- src/main/java/model/Estabelecimento.java
+++ src/main/java/model/Estabelecimento.java
@@ -1,19 +1,44 @@
 package model;
 
+import javax.validation.constraints.NotNull;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Estabelecimento {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "identificador")
+	@NotNull
 	Integer identificador;
 
+	@Column(name = "nome", length = 32, nullable = false)
+	@NotNull
 	String nome;
 
+	@Column(name = "cnpj", length = 16, nullable = false)
+	@NotNull
 	String cnpj;
 
+	@Column(name = "endereco", length = 32, nullable = false)
+	@NotNull
 	String endereco;
 
+	@Column(name = "telefone", length = 16, nullable = false)
+	@NotNull
 	String telefone;
 
+	@Column(name = "quantidade_vagas_motos",nullable = false)
+	@NotNull
 	Integer quantidadeVagasMotos;
 
+	@Column(name = "quantidade_vagas_carros")
+	@NotNull
 	Integer quantidadeVagasCarros;
 
 	public Estabelecimento(String nome, String endereco, Integer quantidadeVagasMotos, Integer quantidadeVagasCarros) {
