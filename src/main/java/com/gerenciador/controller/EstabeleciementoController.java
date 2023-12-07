41dac8dd3e378cabd9645492ca27bd74d9eebb93
FranciscoGJR <franciscoogjr@gmail.com>
2023-12-07 16:45:22 -0300

    feature: implementando EstabelecimentoController.getEstabelecimentoById()

--- src/main/java/com/gerenciador/controller/EstabeleciementoController.java
+++ src/main/java/com/gerenciador/controller/EstabeleciementoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +20,16 @@ public class EstabeleciementoController {
 	private EstabelecimentoService estabelecimentoService;
 	
 	@GetMapping
-	public ResponseEntity<List<Estabelecimento>> getCursos(){
+	public ResponseEntity<List<Estabelecimento>> getEstabelecimento(){
 		List<Estabelecimento> lista = estabelecimentoService.getEstabelecimento();
 		
 		return ResponseEntity.ok().body(lista);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Estabelecimento> getEstabelecimentoById(@PathVariable("id") Integer id){
+		Estabelecimento estabelecimento = estabelecimentoService.findById(id);
+		
+		return ResponseEntity.ok().body(estabelecimento);
 	}
 }
