9e1a1699fe7eaebdad465dd94abc2e2af1ff649c
FranciscoGJR <franciscoogjr@gmail.com>
2023-12-09 21:07:14 -0300

    feacture: implementando VeiculoRepository.buscarVeiculosEmEstacionamento()

--- src/main/java/com/gerenciador/repository/EstabelecimentoRepository.java
+++ src/main/java/com/gerenciador/repository/EstabelecimentoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.gerenciador.model.Estabelecimento;
 
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Integer>{
-
+	
 }
