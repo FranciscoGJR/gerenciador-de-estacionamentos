e11405346e50ada80105c4fcae8e3889b20675fc
FranciscoGJR <franciscoogjr@gmail.com>
2023-12-09 21:28:40 -0300

    fix: corrigindo query em VeiculoRepository.buscarVeiculosEmEstacionamento()

--- src/main/java/com/gerenciador/repository/VeiculoRepository.java
+++ src/main/java/com/gerenciador/repository/VeiculoRepository.java
@@ -1,9 +1,14 @@
 package com.gerenciador.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gerenciador.model.Veiculo;
 
 public interface VeiculoRepository extends JpaRepository<Veiculo, Integer>{
 
+	@Query(value = "SELECT ESTABELECIMENTO_IDENTIFICADOR WHERE VEICULO FROM VEICULO_IDENTIFICADOR = :identificadorVeiculo", nativeQuery = true)
+	Integer buscarVeiculosEmEstacionamento(@Param("identificadorVeiculo") Integer identificadorVeiculo);
+	
 }
